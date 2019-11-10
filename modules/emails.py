1e51defbde85cd075ac5f606395b53a7a2322bee
BuildTools <unconfigured@null.spigotmc.org>
2019-11-10 22:26:00 +0100

    Optymalizacja i czyszczenie kodu

--- modules/emails.py
+++ modules/emails.py
@@ -2,74 +2,45 @@ import os
 import time
 
 emails = []
-d = []
 dirlist = []
 dirlist_f = []
 dirlist_save = []
 cho = []
-rejestr_db_email = []
-rejestr_tmp = []
+registry_db_email = []
+registry_tmp = []
 
 # Komunikaty
 odmowa = "To nie jest adres email. Odmowa zapisu"
 
-def save_1(email):
+def save(email):
     print(emails)
     if email.count(":"):
+        symbol_index = None
         if email.count(";"):
-            c = int(email.index(";"))
-            d.append(c)
-        b = int(email.index(":"))
-        if b == 0:
+            symbol_index = int(email.index(";"))
+        symbol_index_1 = int(email.index(":"))
+        if symbol_index_1 == 0:
             emails.append(email[1:])
         else:
-            if d != []:
-                e = d.pop(0)
-                emails.append(email[b+1:e])
+            if symbol_index is not None:
+                emails.append(email[symbol_index_1+1:symbol_index])
             else:
-                emails.append(email[b+1:])
+                emails.append(email[symbol_index_1+1:])
 
     if email.count("/"):
         if email.count("www.google.com/maps"):
             print(odmowa)
         else:
-            b = int(email.index("/"))
-            if b == 0:
+            symbol_index = int(email.index("/"))
+            if symbol_index == 0:
                 emails.append(email[1:])
             else:
-                emails.append(email[b+1:])
-
-def save_2(email_2):
-    if email_2.count(":"):
-        if email_2.count(";"):
-            c = int(email_2.index(";"))
-            d.append(c)
-        b = int(email_2.index(":"))
-        if b == 0:
-            emails.append(email_2[1:])
-        else:
-            if d != []:
-                e = d.pop(0)
-                emails.append(email_2[b+1:e])
-            else:
-                emails.append(email_2[b+1:])
-
-    if email_2.count("/"):
-        if email_2.count("www.google.com/maps"):
-            print(odmowa)
-        else:
-            b = int(email_2.index("/"))
-            if b == 0:
-                emails.append(email_2[1:])
-            else:
-                emails.append(email_2[b+1:])
-
-    # TODO: Złączenie funkcji save_2 z funkcją save_1 w nową o nazwie save
+                emails.append(email[symbol_index+1:])
 
 def dir_db_save(list):
     os.makedirs("config", exist_ok=True)
     dir_db = open("config/dir_db.txt", "a")
-    while list != []:
+    while list:
         dir_db.write("{} \n".format(list.pop(0)))
     dir_db.close()
 
@@ -80,20 +51,18 @@ def dir_db_read():
     dir_db.close()
 
 def new_directory():
-    dirname = input("Jak chcesz nazwać nowy folder? \n")
-    dirlist_save.append(dirname)
-    cho.append(dirname)
-    os.makedirs("emaile/{}".format(dirname), exist_ok=True)
+    dir_name = input("Jak chcesz nazwać nowy folder? \n")
+    dirlist_save.append(dir_name)
+    cho.append(dir_name)
+    os.makedirs("emaile/{}".format(dir_name), exist_ok=True)
 
 def cho_dir():
-    liczba = 0
-    def g():
-        global liczba
-    for liczba, folder in enumerate(dirlist_f):
-        print("{} - {}".format(liczba, folder))
-    print("{} - Wróć do tworzenia nowego folderu".format(liczba+1))
+    number = 0
+    for number, folder in enumerate(dirlist_f):
+        print("{} - {}".format(number, folder))
+    print("{} - Wróć do tworzenia nowego folderu".format(number+1))
     choose = int(input("Wybierz nazwę folderu \n"))
-    if choose == liczba+1:
+    if choose == number+1:
         new_directory()
     else:
         dirname = dirlist[choose]
@@ -101,36 +70,32 @@ def cho_dir():
         cho.append(dirname[0:n-1])
         os.makedirs("emaile/{}".format(dirname[0:n-1]), exist_ok=True)
 
-    # TODO: Usunięcie zmiennej globalnej g
-
-def rejestr_read():
-    rejestr = open("config/rejestr.txt", "a+")
-    rejestr_read = open("config/rejestr.txt", "r")
-    for email in rejestr_read:
+def registry_read():
+    registry = open("config/rejestr.txt", "r")
+    for email in registry:
         n = email.index("\n")
         email_n = email[0:n-1]
-        rejestr_db_email.append(email_n)
-    rejestr.close()
+        registry_db_email.append(email_n)
+    registry.close()
 
-def rejestr_save(list):
-    rejestr = open("config/rejestr.txt", "r")
-    rejestr_save = open("config/rejestr.txt", "a+")
-    for wpis in rejestr:
+def registry_save(list):
+    registry = open("config/rejestr.txt", "r+")
+    for wpis in registry:
         n = wpis.index("\n")
         wpis_n = wpis[0:n-1]
-        rejestr_tmp.append(wpis_n)
+        registry_tmp.append(wpis_n)
     for email in list:
-        if rejestr_tmp.count(email) > 0:
+        if registry_tmp.count(email) > 0:
             print("INFORMACJA: Adres jest już w rejestrze")
         else:
             position = emails.index(email)
-            rejestr_save.write("{} \n".format(emails[position]))
-    rejestr_save.close()
+            registry.write("{} \n".format(emails[position]))
+    registry.close()
 
 def file():
     try:
         dir_db_read()
-    except:
+    except FileNotFoundError:
         print("Plik konfiguracji nie istnieje")
     for dir in dirlist:
         if dir.count("\n"):
@@ -155,7 +120,7 @@ def file():
     dir_db_save(dirlist_save)
     f = open("emaile/{}/email {}.txt".format(cho.pop(0), data), "a")
     print("Plik zapisu w trakcie tworzenia...")
-    rejestr_read()
+    registry_read()
     for email in emails:
         if email.count("@"):
             if email.count("\\"):
@@ -171,14 +136,14 @@ def file():
             if email.count("&"):
                 print(odmowa)
             else:
-                if rejestr_db_email.count(email) > 0:
+                if registry_db_email.count(email) > 0:
                     print("INFORMACJA: Adres jest już w rejestrze")
                 else:
                     f.write("{} \n".format(email))
         else:
             print(odmowa)
     f.close()
-    rejestr_save(emails)
+    registry_save(emails)
     print("Plik został pomyślnie utworzony")
 
     while dirlist != [] and dirlist_f != []:

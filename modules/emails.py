38abaf190bb89cf469d820b761ee8b4f573fca3f
BuildTools <unconfigured@null.spigotmc.org>
2019-11-13 19:18:06 +0100

    Optymalizacja

--- modules/emails.py
+++ modules/emails.py
@@ -3,17 +3,13 @@ import time
 
 emails = []
 dirlist = []
-dirlist_f = []
-dirlist_save = []
-cho = []
-registry_db_email = []
-registry_tmp = []
+registry_db = []
+tmp = {}
 
 # Komunikaty
 odmowa = "To nie jest adres email. Odmowa zapisu"
 
 def save(email):
-    print(emails)
     if email.count(":"):
         symbol_index = None
         if email.count(";"):
@@ -37,11 +33,10 @@ def save(email):
             else:
                 emails.append(email[symbol_index+1:])
 
-def dir_db_save(list):
+def dir_db_save():
     os.makedirs("config", exist_ok=True)
     dir_db = open("config/dir_db.txt", "a")
-    while list:
-        dir_db.write("{} \n".format(list.pop(0)))
+    dir_db.write("{} \n".format(tmp["new_dir"]))
     dir_db.close()
 
 def dir_db_read():
@@ -51,41 +46,43 @@ def dir_db_read():
     dir_db.close()
 
 def new_directory():
-    dir_name = input("Jak chcesz nazwać nowy folder? \n")
-    dirlist_save.append(dir_name)
-    cho.append(dir_name)
-    os.makedirs("emaile/{}".format(dir_name), exist_ok=True)
+    dirname = input("Jak chcesz nazwać nowy folder? \n")
+    tmp["new_dir"] = dirname
+    tmp["cho"] = dirname
+    os.makedirs("emaile/{}".format(dirname), exist_ok=True)
 
 def cho_dir():
     number = 0
-    for number, folder in enumerate(dirlist_f):
-        print("{} - {}".format(number, folder))
-    print("{} - Wróć do tworzenia nowego folderu".format(number+1))
-    choose = int(input("Wybierz nazwę folderu \n"))
-    if choose == number+1:
-        new_directory()
+    if dirlist:
+        for number, folder in enumerate(dirlist):
+            n = folder.index("\n")
+            folder_n = folder[0:n - 1]
+            print("{} - {}".format(number, folder_n))
+        print("{} - Wróć do tworzenia nowego folderu".format(number+1))
+        choose = int(input("Wybierz nazwę folderu \n"))
+        if choose == number+1:
+            new_directory()
+        else:
+            dirname = dirlist[choose]
+            n = dirname.index("\n")
+            tmp["cho"] = dirname[0:n-1]
+            os.makedirs("emaile/{}".format(dirname[0:n-1]), exist_ok=True)
     else:
-        dirname = dirlist[choose]
-        n = dirname.index("\n")
-        cho.append(dirname[0:n-1])
-        os.makedirs("emaile/{}".format(dirname[0:n-1]), exist_ok=True)
+        print("INFORMACJA: Lista folderów jest pusta!")
+        new_directory()
 
 def registry_read():
     registry = open("config/rejestr.txt", "r")
     for email in registry:
         n = email.index("\n")
         email_n = email[0:n-1]
-        registry_db_email.append(email_n)
+        registry_db.append(email_n)
     registry.close()
 
 def registry_save(list):
     registry = open("config/rejestr.txt", "r+")
-    for wpis in registry:
-        n = wpis.index("\n")
-        wpis_n = wpis[0:n-1]
-        registry_tmp.append(wpis_n)
     for email in list:
-        if registry_tmp.count(email) > 0:
+        if email in registry_db:
             print("INFORMACJA: Adres jest już w rejestrze")
         else:
             position = emails.index(email)
@@ -97,28 +94,22 @@ def file():
         dir_db_read()
     except FileNotFoundError:
         print("Plik konfiguracji nie istnieje")
-    for dir in dirlist:
-        if dir.count("\n"):
-            n = dir.index("\n")
-            dirlist_f.append(dir[0:n-1])
     os.makedirs("emaile", exist_ok=True)
     print("Katalog został utworzony")
-    l = list(os.listdir("emaile"))
-    if l == [] and dirlist == []:
+    files_list = list(os.listdir("emaile"))
+    if files_list == [] and dirlist == []:
         new_directory()
     else:
-        decyzja = int(input("Co chcesz zrobić? \n 1 - Wybrać nazwę folderu z zapisanych \n 2 - Utworzyć nowy \n"))
-        if decyzja == 1:
-            try:
-                cho_dir()
-            except:
-             print("BŁĄÐ: Lista folderów jest pusta")
+        decision = int(input("Co chcesz zrobić? \n 1 - Wybrać nazwę folderu z zapisanych \n 2 - Utworzyć nowy \n"))
+        if decision == 1:
+            cho_dir()
         else:
             new_directory()
 
     data = time.strftime("%H:%M %d.%m.%Y")
-    dir_db_save(dirlist_save)
-    f = open("emaile/{}/email {}.txt".format(cho.pop(0), data), "a")
+    if "new_dir" in tmp.keys():
+        dir_db_save()
+    f = open("emaile/{}/email {}.txt".format(tmp["cho"], data), "a")
     print("Plik zapisu w trakcie tworzenia...")
     registry_read()
     for email in emails:
@@ -136,7 +127,7 @@ def file():
             if email.count("&"):
                 print(odmowa)
             else:
-                if registry_db_email.count(email) > 0:
+                if email in registry_db:
                     print("INFORMACJA: Adres jest już w rejestrze")
                 else:
                     f.write("{} \n".format(email))
@@ -146,11 +137,9 @@ def file():
     registry_save(emails)
     print("Plik został pomyślnie utworzony")
 
-    while dirlist != [] and dirlist_f != []:
+    while dirlist:
         del dirlist[0]
-        del dirlist_f[0]
     print("Pamięć podręczna została wyczyszczona")
 
-# TODO: Uproszczenie struktury, przede wszystkim zmniejszenie ilości list
 # TODO: Stworzenie klasy, w której skład wejdą funkcję odpowiadające za operacje na plikach
 # TODO: Nowy sposób zapisu plików z podziałem na poszczególne strony i znalezione na nich e-maile + numery telefonów
--- modules/filtr.py
+++ modules/filtr.py
@@ -4,40 +4,38 @@ cho_dir = []
 cho_file = []
 
 def menu_dir():
-    liczba = 0
+    number = 0
     listdir = os.listdir("emaile")
-    for liczba, dir in enumerate(listdir):
-        print("{} - {}".format(liczba, dir))
+    for number, dir in enumerate(listdir):
+        print("{} - {}".format(number, dir))
     choose = int(input("Wybierz folder\n"))
     cho_dir.append(listdir[choose])
 
     listdir = os.listdir("emaile/{}".format(cho_dir[0]))
-    for liczba, file in enumerate(listdir):
-        print("{} - {}".format(liczba, file))
-    print("{} - Powrót do wyboru folderów".format(liczba+1))
+    for number, file in enumerate(listdir):
+        print("{} - {}".format(number, file))
+    print("{} - Powrót do wyboru folderów".format(number+1))
     choose = int(input("Wybierz plik, który chcesz przefiltrować\n"))
-    if choose == liczba+1:
+    if choose == number+1:
         menu_dir()
     else:
         cho_file.append(listdir[choose])
 
-def filter():
+def email_filter():
     menu_dir()
-    f = open("emaile/{}/{}".format(cho_dir[0], cho_file[0]), "r+").readlines()
-    plik = open("emaile/{}/email_filtr {}".format(cho_dir.pop(0), cho_file.pop(0)), "w+")
+    file = open("emaile/{}/{}".format(cho_dir[0], cho_file[0]), "r+").readlines()
+    file_filter = open("emaile/{}/email_filtr {}".format(cho_dir.pop(0), cho_file.pop(0)), "w+")
     tmp_db = []
 
-    for phrase in f:
+    for phrase in file:
         tmp_db.append(phrase)
 
-    while tmp_db != []:
+    while tmp_db:
         current = tmp_db.pop(0)
         if current.count("//" or "facebook.com" or "google.com" or "google.pl"):
             print("To nie jest adres email")
         else:
-            plik.write("{} \n".format(current))
+            file_filter.write("{} \n".format(current))
 
-    print("Filtracja zakończona pomyślnie")
-    plik.close()
-
-# TODO: Przejrzenie kodu
+    print("Filtracja zakończona pomyślnie!")
+    file_filter.close()
